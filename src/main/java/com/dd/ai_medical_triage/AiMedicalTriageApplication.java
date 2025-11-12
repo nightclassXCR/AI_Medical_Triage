d581dbcb2caab900622ddc4a91d35ec3ea305767
nightclassXCR <1246716434@qq.com>
2025-11-12 19:13:16 +0800

    FEAT:初始化

---
+++ src/main/java/com/dd/ai_medical_triage/AiMedicalTriageApplication.java
@@ -0,0 +1,12 @@
+package com.dd.ai_medical_triage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMedicalTriageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AiMedicalTriageApplication.class, args);
+    }
+
+}

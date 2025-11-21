39b8fa1949298d4d5e5814a68ed58900694f2d25
lcz2799707969@gmail.com <lcz2799707969@gmail.com>
2025-11-21 19:40:10 +0800

    feat:1

---
+++ src/main/java/com/dd/ai_medical_triage/dto/tool/AppointmentMQDTO.java
@@ -0,0 +1,14 @@
+package com.dd.ai_medical_triage.dto.tool;
+
+
+import lombok.Data;
+
+@Data
+public class AppointmentMQDTO {
+    private String msgId;        // 用于 Redis 幂等
+    private Long appointmentId;  // 挂号表主键
+    private Long patientId;
+    private Long doctorId;
+    private String symptoms;
+    private String priority;
+}
